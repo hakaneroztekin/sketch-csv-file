be35cc4da249cb2053c2996bfdc932e1b4c96b50
hakaneroztekin <eroztekin@itu.edu.tr>
2019-02-24 09:58:49 +0300

    add reading all the csv files

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # 24 Feb 2019
 #
 # Implementation Plan
-# Read csv files (the requested columns) into arrays
+# Read csv files (the requested columns) into lists
 # Sketch the csv files.
 # Beautify the graph if needed.
 
@@ -20,30 +20,47 @@ from bs4 import BeautifulSoup
 import csv
 
 class App:
-    source = ""
+    column = []
+    row = []
+
     def __init__(self):
-        x = []
-        y = []
-        self.read_csv()
+        # Csv file content:
+        # Time, L:Measurement, L:ConvertedCap, L:Angle, R:Measurement, R:ConvertedCap, R:Angle
+
+        self.read_all_csv()
         # sketch()
 
+    def read_all_csv(self):
+        input_val = input("How many files do you want to read? ")
+        i = 1
+        while i < int(input_val):
+            self.read_csv(i)
+            i += 1
+            print("All files are read")
 
-    def read_csv(self): #read csv
-        with open('csv_files/measurement1.csv') as csv_file:
+    def read_csv(self, i):  # read csv
+        source = "csv_files"
+        file_name = "measurement" + str(i) + ".csv"
+        full_address = source + "/" + file_name
+
+        with open(full_address) as csv_file:
+            print("Reading file: " + file_name)
             csv_reader = csv.reader(csv_file, delimiter=',')
             line_count = 0
+
             for row in csv_reader:
                 if line_count == 0:
                     #print(f'Column names are {", ".join(row)}')
                     line_count += 1
                 else:
                     #print(f'\t time:{row[0]}  Lcap: {row[1]} Ldeg: {row[2]}.')
+                    self.row.append(row)
                     line_count += 1
-            #print(f'Processed {line_count} lines.')
+            print(f'Processed {line_count} lines.')
 
 
 
-    # def sketch(self): #sketch csv
+    # def sketch(self):  # sketch csv
 
 
 
