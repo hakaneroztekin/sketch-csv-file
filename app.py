10b37e8a54385babb72a4b7be7bf6e98cc753b4c
hakaneroztekin <eroztekin@itu.edu.tr>
2019-02-24 09:34:20 +0300

    add dependencies

--- app.py
+++ app.py
@@ -5,3 +5,13 @@
 # Read csv files (the requested columns) into arrays
 # Sketch the csv files.
 # Beautify the graph if needed.
+
+import re
+from contextlib import closing
+
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from bs4 import BeautifulSoup
+from requests import get
+from requests.exceptions import RequestException
+
